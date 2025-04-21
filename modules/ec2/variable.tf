9bd0ef97421240e37d77d8f04e2e6f629cd10cf9
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-04-21 07:25:46 +0530

    Update variable.tf

--- modules/ec2/variable.tf
+++ modules/ec2/variable.tf
@@ -1,4 +1,4 @@
-mvariable "ami_value" {
+variable "ami_value" {
   description = "The AMI ID to use for the instance"
   type        = string
   
@@ -7,4 +7,4 @@ mvariable "ami_value" {
 variable "instance_type" {
   description = "The type of instance to create"
   type        = string
-}v
+}
